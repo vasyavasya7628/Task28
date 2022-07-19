ac8b8f9ab77b2af99145274486ba44a2102373c7
Vasily7628 <mineok44@gmail.com>
2022-07-19 23:10:31 +0700

    Вызвал фрагмент через код

--- app/src/main/java/com/example/task28phones/presentation/MainActivity.kt
+++ app/src/main/java/com/example/task28phones/presentation/MainActivity.kt
@@ -7,9 +7,16 @@ import com.example.task28phones.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val phonesFragment: PhonesFragment = PhonesFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, phonesFragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
